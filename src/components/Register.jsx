09e616a8a775a09b11120a7779b4d93d96b3f7b8
caner-baysal <canerbaysal@hotmail.com>
2025-08-03 14:46:11 +0300

    second commit

---
+++ src/components/Register.jsx
@@ -0,0 +1,183 @@
+import { useEffect, useState } from "react";
+import { Button, Card, CardBody, CardFooter, CardHeader, Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
+import axios from "axios";
+
+const initialValues = {
+    ad: "",
+    soyad: "",
+    email: "",
+    password: "",
+}
+const errorMessages = {
+    ad: "Adınız en z 3 karakter olmalıdır",
+    soyad: "Soyadınız en az 3 karakter olmalıdır",
+    email: "Geçerli bir email adresi giriniz",
+    password: "En az 8 karakter, en az 1 büyük harf, en az 1 küçük harf, en az 1 sembol ve en az 1 rakam içermelidir;"
+};
+
+const [id, SetID] = useState("");
+
+export default function Register() {
+    const [formData, setFormData] = useState(initialValues);
+    const [errors, setErrors] = useState({
+        ad: false,
+        soyad: false,
+        email: false,
+        password: false,
+    })
+    const[isValid, setIsValid] = useState(false);
+
+    const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    useEffect(() => {
+        if(
+            formData.ad.trim().length >= 3 &&
+            formData.soyad.trim().length >= 3 &&
+           validateEmail(formData.email) &&
+            regex.test(formData.password)
+        ) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+    }, [formData])
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData({...formData, [name]: value});
+        if(name == "ad" || name == "soyad") {
+            if(value.trim().length >= 3){
+                setErrors({...errors, [name]: false});
+            } else {
+                setErrors({...errors, [name]: true});
+            }      
+        }
+        if(name == "email") {
+            if(validateEmail(value)) {
+                setErrors({...errors, [name]: false});
+            } else {
+                setErrors({...errors, [name]: true});
+            }
+
+        }
+
+        if(name == "password") {
+            if(regex.test(value)) {
+                setErrors({...errors, [name]: false});
+            } else {
+                setErrors({...errors, [name]: true});
+            }
+        }
+
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(!isValid) return;
+        axios.post("https://regres.in/api/users", formData)
+        .then(response=>{
+            SetID(response.data.id);
+            setFormData(initialValues);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+    return(
+    <Card>
+        <CardHeader>Kayıt</CardHeader>
+        <CardBody>
+       <Form onSubmit={handleSubmit}>
+    <FormGroup>
+    <Label for="ad">
+      Ad:
+    </Label>
+    <Input
+      id="ad"
+      name="ad"
+      placeholder="Adınızı yazınız"
+      type="text"
+      onChange={handleChange}
+      value={formData.ad}
+      invalid={errors.ad}
+      data-cy="ad-input"
+    />
+    {errors.ad && <FormFeedback data-cy="error-message">
+      {errorMessages.ad}
+    </FormFeedback>}
+    <FormFeedback>
+      Oh noes! that name is already taken
+    </FormFeedback>
+  </FormGroup>
+    <FormGroup>
+    <Label for="soyad">
+      Soyad:
+    </Label>
+    <Input
+      id="soyad"
+      name="soyad"
+      placeholder="Soyadınızı yazınız"
+      type="text"
+      onChange={handleChange}
+      value={formData.soyad}
+      invalid={errors.soyad}
+      data-cy="soyad-input"
+    />
+    {errors.soyad && <FormFeedback data-cy="error-message"  >
+      {errorMessages.soyad}
+    </FormFeedback>}
+  </FormGroup>      
+  <FormGroup>
+    <Label for="exampleEmailenail">
+      Email:
+    </Label>
+    <Input
+      id="email"
+      name="email"
+      placeholder="Email adresinizi yazınız"
+      type="email"
+      onChange={handleChange}
+      value={formData.email}
+      invalid={errors.email}
+      data-cy="email-input"
+    />
+    {errors.email && <FormFeedback data-cy="error-message">
+      {errorMessages.email}
+    </FormFeedback>}
+  </FormGroup>
+  <FormGroup>
+    <Label for="password">
+      Password:
+    </Label>
+    <Input
+      id="password"
+      name="password"
+      placeholder="Güçlü bir password giriniz"
+      type="password"
+      onChange={handleChange}
+      value={formData.password}
+      invalid={errors.password}
+      data-cy="password-input"
+    />
+    {errors.password && <FormFeedback data-cy="error-message">
+      {errorMessages.password}
+    </FormFeedback>}
+  </FormGroup>
+
+  <Button disabled={!isValid}
+  data-cy="submit-buton">
+    Kayıt Ol
+  </Button>
+</Form>
+</CardBody>
+<CardFooter>ID: {id}</CardFooter>
+</Card>
+    )
+}
